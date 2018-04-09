e370827ce4b10ce3e93ae643926aa58899b88d13
Marcus Turnbo <marcus@emarcus.com>
2018-04-08 21:59:55 -0500

    Added filter indicators

--- src/containers/FilterContainer/FilterContainer.jsx
+++ src/containers/FilterContainer/FilterContainer.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './FilterContainer.scss';
 import TextFilter from '../../components/TextFilter';
 import CheckFilter from '../../components/CheckFilter';
+import FilterIndicator from '../../components/FilterIndicator';
+import DataTable from '../../components/DataTable';
+import data from '../../../data/persons.json';
 
 class FilterContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      columns: Object.keys(data[0]),
+      originalData: data,
+      filteredData: data,
       filters: []
     };
   }
@@ -15,11 +21,15 @@ class FilterContainer extends React.Component {
     e.preventDefault();
     const filters = this.state.filters;
     this.setState({
-      filters: [...this.state.filters, {field: e.target.field.value, value: e.target.filter.value}]
+      filters: [...this.state.filters, {field: e .target.field.value, value: e.target.filter.value}]
     });
   }
 
   render() {
+    const selectedFilters = this.state.filters.map(filter =>
+      <FilterIndicator label={filter.field} removeFilterHandler={e => this.removeFilter(e)} />
+    );
+
     return (
       <div className="app-container">
         <h3>React Filters</h3>
@@ -30,6 +40,11 @@ class FilterContainer extends React.Component {
           <CheckFilter field="gender" value="Male" label="Male" handleSubmit={e => this.handleFilterSubmit(e)} />
           <CheckFilter field="gender" value="Female" label="Female" handleSubmit={e => this.handleFilterSubmit(e)} />
         </div>
+        <div>
+          {selectedFilters}
+        </div>
+        <hr />
+        <DataTable columns={this.state.columns} data={this.state.filteredData} />
       </div>
     );
   }
